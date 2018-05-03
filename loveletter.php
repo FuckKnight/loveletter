7703f1c0218ec093a448e59cbd2a0d29c389258d
FuckKnight <37451042+FuckKnight@users.noreply.github.com>
2018-05-03 18:49:18 +0800

    Update loveletter.php

--- loveletter.php
+++ loveletter.php
@@ -3,11 +3,11 @@
     {
 		$postdata = http_build_query($data);
 		$opts = array('http' =>
-						  array(
-							  'method' => 'POST',
-							  'header' => 'Content-type: application/x-www-form-urlencoded',
-							  'content' => $postdata
-						  )
+			array(
+				'method' => 'POST',
+				'header' => 'Content-type: application/x-www-form-urlencoded',
+				'content' => $postdata
+			)
 		);
 		$context = stream_context_create($opts);
 		$result = file_get_contents($url, false, $context);
